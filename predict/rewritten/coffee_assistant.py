f10efa61d5423171591274573930cfb4233f4b46
Breno de Angelo <brenodeangelo@gmail.com>
2023-12-18 22:16:40 -0300

    Exemplo funcional

--- predict/rewritten/coffee_assistant.py
+++ predict/rewritten/coffee_assistant.py
@@ -1,9 +1,11 @@
 from openai import OpenAI
-from multiprocessing import Process
+from threading import Thread, Event
+from queue import Queue
 import argparse
-import cv2
 from agent import GPTVisionAgent
 from image_handler import ImageHandler
+from time import sleep
+from cv2 import waitKey
 
 
 system_prompt = (
@@ -15,9 +17,9 @@ system_prompt = (
     "fornecendo instruções sequenciais para o preparo do café, considere que será usado café em pó.\n"
     "Você deve ser capaz de guiar um usuário que nunca preparou café antes,"
     "sempre pergunte se o usuário tem o item necessário para a tarefa e se o item é próprio para a tarefa, "
-    "só prossiga com a tarefa se o usuário confirmar que tem o item.\n"                                    
+    "só prossiga com a tarefa se o usuário confirmar que tem o item.\n"
     "Suas instruções serão claras e diretas, não mais do que uma tarefa por vez e limite de 100 caracteres por tarefa. "
-    "Exemplos de interações:\n" 
+    "Exemplos de interações:\n"
     "(EXEMPLO)'user': 'Clio, me pergunte se podemos iniciar'; 'system': 'Podemos iniciar o preparo do café?'; 'user': 'Sim';\n"
     "(EXEMPLO)'system': 'Verifique se você tem um recipiente para ferver a água\n"
     "(EXEMPLO)'user': 'Passo concluído.'; 'system': 'Encontre uma torneira'\n"
@@ -25,35 +27,55 @@ system_prompt = (
 )
 
 
-def main(ip_address):
+def show_webcam():
+    camera_handler = ImageHandler(ip_address)
+    while True:
+        if capture_frame_event.is_set():
+            capture_frame_event.clear()
+            queue.put(camera_handler.capture_webcam_frame())
+            frame_captured_event.set()
+        camera_handler.show_webcam()
+        
+
+def main():
     client = OpenAI()
     coffee_assistant = GPTVisionAgent(system_prompt=system_prompt,
                                       model="gpt-4-vision-preview",
                                       image_history_rule='none')
-    img_handler = ImageHandler(ip_address)
-
-    process_show_webcam = Process(target=img_handler.show_webcam)
-    process_show_webcam.start()
-
     user_response = (
-        "Eu irei fazer uma demo testando através de imagens na tela do meu computador, considere-as como" 
+        "Eu irei fazer uma demo testando através de imagens na tela do meu computador, considere-as como"
         "'reais' para fins de teste. Me pergunte se podemos iniciar"
     )
     frame_count = 0
     while True:
-        image = img_handler.capture_webcam_frame(frame_count)
+        user_response = input("teste")  # Wait for user response from the main process
+        for i in range(3):
+            sleep(0.75)
+            print(f"Capturing frame in {3 - i}...")
+        capture_frame_event.set()
+        frame_captured_event.wait()
+        image = queue.get()
         response = coffee_assistant.get_response(client, image, user_response)
         print(response)
-        user_response = input("Escreva sua resposta:\n")
         frame_count += 1
-
-    process_show_webcam.join()
 
 
 if __name__ == "__main__":
-    
-    parser = argparse.ArgumentParser(description="A program that does something.")
-    parser.add_argument('ip_address', type=str, help='The camera IP address to use')
+
+    parser = argparse.ArgumentParser(
+        description="A program that does something.")
+    parser.add_argument('ip_address', type=str,
+                        help='The camera IP address to use')
     args = parser.parse_args()
     ip_address = args.ip_address
-    main(ip_address)
+
+    capture_frame_event = Event()
+    frame_captured_event = Event()
+    queue = Queue()
+
+    thread_show_webcam = Thread(target=show_webcam)
+    thread_show_webcam.start()
+
+    main()
+
+    thread_show_webcam.join()
--- predict/rewritten/image_handler.py
+++ predict/rewritten/image_handler.py
@@ -6,21 +6,32 @@ import base64
 
 class ImageHandler():
     cap: cv2.VideoCapture
+    frame_count: int = 0
     
     def __init__(self, ip_address: str) -> None:
         self.cap = cv2.VideoCapture(ip_address)
+        self.clear_captured_images_directory()
+
+    def clear_captured_images_directory(self):
+        directory = os.path.join(os.path.dirname(__file__), '..', 'CapturedImages')
+        if os.path.exists(directory):
+            for file in os.listdir(directory):
+                file_path = os.path.join(directory, file)
+                try:
+                    if os.path.isfile(file_path):
+                        os.unlink(file_path)
+                except Exception as e:
+                    print(e)
     
     def show_webcam(self) -> None:
-        while True:
-            ret, frame = self.cap.read()
-            if ret:
-                cv2.imshow('Webcam', frame)
-            if cv2.waitKey(10) & 0xFF == ord('q'):
-                break
-        self.cap.release()
-        cv2.destroyAllWindows()
-
-    def capture_webcam_frame(self, frame_count):
+        ret, frame = self.cap.read()
+        if ret:
+            cv2.imshow('Webcam', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            return False
+        return True
+    
+    def capture_webcam_frame(self):
         # Capture a frame from the webcam
         ret, frame = self.cap.read()
 
@@ -33,14 +44,17 @@ class ImageHandler():
             os.makedirs(base_path)
 
         # Define the path to the file where the captured frame will be saved
-        filename = os.path.join(base_path, f"passo_{frame_count}.jpg")
+        filename = os.path.join(base_path, f"passo_{self.frame_count}.jpg")
 
         # Save the captured frame to the file
         cv2.imwrite(filename, frame)
 
         # Print the path to the saved file
         print(f"Frame saved to: {filename}")
-        
+
+        # Increment the frame count
+        self.frame_count += 1
+
         # Return the captured frame
         return self.encode_image(filename)
     
